0a58debd1891f7ac5726811e5846a33b82829682
luvofu <380843872@qq.com>
2018-07-03 22:19:28 +0800

    push resources
    zm

--- books/src/main/java/com/wg/admin/webservice/TestController.java
+++ books/src/main/java/com/wg/admin/webservice/TestController.java
@@ -3,8 +3,8 @@ package com.wg.admin.webservice;
 import com.alibaba.fastjson.JSON;
 import com.wg.admin.dao.TestDao;
 import com.wg.admin.model.request.TestRequest;
+import com.wg.book.dao.BookDao;
 import com.wg.common.ResponseContent;
-import com.wg.common.utils.export.PdfUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileNotFoundException;
 
 /**
  * Created by Administrator on 2017/3/3 0003.
@@ -24,16 +23,22 @@ public class TestController {
     @Autowired
     TestDao testDao;
 
+    @Autowired
+    BookDao bookDao;
+
     @Transactional
     @RequestMapping(value = "api/test/test", produces = "application/json;charset=utf-8"/*, method = RequestMethod.POST*/)
     @ResponseBody
     public String test(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap, TestRequest testRequest) {
         ResponseContent responseContent = new ResponseContent();
-        try {
-            PdfUtils.createPdf();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            PdfUtils.createPdf();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+
+
         return JSON.toJSONString(responseContent);
     }
 }
